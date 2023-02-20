9a227c0cac45fe67b54303629d0f1eea742aa422
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2023-02-20 19:16:39 +0000

    added ms timer to render

--- src/components/QuoteText.js
+++ src/components/QuoteText.js
@@ -6,7 +6,8 @@ const QuoteText = ({sentence, author}) => {
 
     // Accuracy counters
     const [errors, setErrors] = useState(0);
-    const [lettersTyped, setLettersTyped] = useState(0);
+    const [words, setWords] = useState(sentence.split(' '));
+    const [wordCount, setWordCount] = useState(0);
 
     // Stop watch
     const [isStarted, setIsStarted] = useState(false);
@@ -15,6 +16,7 @@ const QuoteText = ({sentence, author}) => {
     useEffect(() => {
         let interval;
         const checkRate = 100;
+
         if (isStarted) {
             interval = setInterval(() => {
                 setTime((prev) => prev + checkRate);
@@ -26,9 +28,8 @@ const QuoteText = ({sentence, author}) => {
     }, [isStarted])
     
     
-    const quoteChars = sentence.split('');
-    const words = sentence.split(' ');
     
+    const quoteChars = sentence.split('');
     const [quoteSpan, setQuoteSpan] = useState(sentence.split('').map((char, index) => {
         return(
             <span key={index} className="default">{char}</span>
@@ -38,20 +39,21 @@ const QuoteText = ({sentence, author}) => {
 
     const processUserText = (event) => {
         let input = event.target.value;
-        setIsStarted(true);
-        console.log(isStarted);
-
-        // Start the timer once the user starts typing
-        // if (!isStarted) {
-        // }
-
-        // while (isStarted) {
+        if (isStarted === false) {
+            setIsStarted(true);
+            console.log(isStarted);
+            // console.log(isStarted);
+        } 
+        
+        // if (isStarted) {
         checkCorrect(input);
         // }
+        
     };
 
     const checkCorrect = (input) => {
         const charArrayTyped = input.split('');
+        const typedWords = input.split(' ');
         const charArray = [...quoteSpan];
         
         // setLettersTyped(lettersTyped + 1);
@@ -70,24 +72,31 @@ const QuoteText = ({sentence, author}) => {
                 charSpan[index] = <span key={index} className="correct">{char.props.children}</span>
                 
             } else {
-                // errors++;
                 charSpan[index] = <span key={index} className="incorrect">{char.props.children}</span>
                 
             }
         })
 
         setQuoteSpan(charSpan);
+
+        if (charArrayTyped.length === quoteChars.length) {
+            setIsStarted(false);
+        }
     };
 
+    const startCountdown = () => {
+
+    };
 
     return(
         <>
             <div className="quoteBox">
                 {sentence ? <p>"{quoteSpan}" - {author}</p> : <p>Loading quote...</p>}
             </div>
-            <textarea placeholder="Click here to start" onChange={(event) => processUserText(event)}>
+            <p>{}</p>
+            <textarea placeholder="Click here to start" onClick={startCountdown} onChange={!isStarted ? (event) => processUserText(event) : () => {}}>
             </textarea>
-            <span>Words per min: {time / words.length}</span>
+            <span>Time: {time.toString().slice(0, -3)}.{time.toString().slice(-3)[0]} ms</span>
         </>
     );
 }
